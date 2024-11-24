26a137d90ab11882b8673ba0a0dbc6e4977dae6d
Dichan Shrestha <blackuniverse.808@gmail.com>
2024-11-24 20:27:17 +0545

    placeholder page managed

--- src/components/ui/placeholders-and-vanish-input.tsx
+++ src/components/ui/placeholders-and-vanish-input.tsx
@@ -77,9 +77,9 @@ export function PlaceholdersAndVanishInput({
     const newData: Particle[] = [];
 
     for (let t = 0; t < 800; t++) {
-      let i = 4 * t * 800;
+      const i = 4 * t * 800;
       for (let n = 0; n < 800; n++) {
-        let e = i + 4 * n;
+        const e = i + 4 * n;
         if (
           pixelData[e] !== 0 &&
           pixelData[e + 1] !== 0 &&
